ae601e58b5453020d4405e5ef9fe08dd91650457
Xonwi <julian.gaggl@gmx.at>
2019-05-13 18:28:10 +0200

    rearange project structure for testing

---
+++ core/src/test/java/com/mygdx/game/GameBoardTest.java
@@ -0,0 +1,21 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.network.GameClient;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class GameBoardTest {
+
+    @Test
+    public void testTest() throws Exception {
+    }
+}
