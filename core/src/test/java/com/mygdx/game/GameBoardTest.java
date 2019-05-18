bbe68d40fd6942f6ad13d76542dadb8779e082f9
juliangaggl <julian.gaggl@gmx.at>
2019-05-18 23:15:31 +0200

    change structure, add tests and sonar-coverage

---
+++ core/src/test/java/com/mygdx/game/GameBoardTest.java
@@ -0,0 +1,10 @@
+package com.mygdx.game;
+import org.junit.Test;
+
+public class GameBoardTest {
+
+    @Test
+    public void testTest() throws Exception {
+
+    }
+}
---
+++ core/src/test/java/com/mygdx/game/TileActorTest.java
@@ -0,0 +1,52 @@
+package com.mygdx.game;
+
+import com.mygdx.game.actor.TileActor;
+import com.mygdx.game.tile.Road;
+import com.mygdx.game.tile.Side;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class TileActorTest {
+
+    TileActor tile = new TileActor(new Position(0, 0), null);
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRotation() {
+        for (int i = 0; i < 5; ++i) tile.rotate();
+        assertSame(tile.getSideAfterRotation(Side.top), Side.right);
+        assertSame(tile.getSideAfterRotation(Side.right), Side.bottom);
+        assertSame(tile.getSideAfterRotation(Side.bottom), Side.left);
+        assertSame(tile.getSideAfterRotation(Side.left), Side.top);
+        assertSame(tile.getTileSideAt(Side.top), Side.left);
+        assertSame(tile.getTileSideAt(Side.right), Side.top);
+        assertSame(tile.getTileSideAt(Side.bottom), Side.right);
+        assertSame(tile.getTileSideAt(Side.left), Side.bottom);
+    }
+
+    @Test
+    public void testAddFeature() {
+        Road road = new Road(Arrays.asList(Side.top, Side.bottom));
+        tile.addFeature(road);
+        assertEquals(tile.getFeatureAtSide(Side.top), road);
+        assertEquals(tile.getFeatureAtSide(Side.bottom), road);
+        assertNull(tile.getFeatureAtSide(Side.right));
+    }
+
+    @Test(expected=Exception.class)
+    public void testAddFeatureException() {
+        Road road1 = new Road(Arrays.asList(Side.top, Side.bottom));
+        Road road2 = new Road(Arrays.asList(Side.top, Side.right));
+        tile.addFeature(road1);
+        tile.addFeature(road2);
+    }
+}
