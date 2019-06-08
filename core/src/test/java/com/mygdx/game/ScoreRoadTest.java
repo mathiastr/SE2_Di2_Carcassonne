42b2aa416899bf995b424120c4a75026492a9b46
j <julian.gaggl@gmx.at>
2019-06-08 21:40:08 +0200

    add missing import

--- core/src/test/java/com/mygdx/game/ScoreRoadTest.java
+++ core/src/test/java/com/mygdx/game/ScoreRoadTest.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.mygdx.game.actor.TileActor;
 import com.mygdx.game.meeple.Meeple;
+import com.mygdx.game.screen.GameScreen;
 import com.mygdx.game.tile.Road;
 import com.mygdx.game.tile.Side;
 
