40a9a1bada4e0128e46aff1c73f53154f9bba80d
Cymbre <peter.waysocher@gmail.com>
2019-06-18 15:31:53 +0200

    added endofTurn Test + cheat test for 3 players + detect cheating after 3 turns

--- core/src/test/java/com/mygdx/game/CheatTest.java
+++ core/src/test/java/com/mygdx/game/CheatTest.java
@@ -4,10 +4,15 @@ import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.mygdx.game.actor.TileActor;
 import com.mygdx.game.network.response.CheatType;
+import com.mygdx.game.network.response.TurnEndMessage;
 import com.mygdx.game.screen.GameScreen;
 import com.mygdx.game.utility.GraphicsBackend;
+import com.mygdx.game.utility.GraphicsBackendForTests;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -26,7 +31,9 @@ public class CheatTest {
         stageMock = mock(Stage.class);
         Gdx.files = mock(Files.class);
         Gdx.gl = mock(GL20.class);
+        Gdx.gl20 = mock(GL20.class);
         gameScreen = mock(GameScreen.class);
+        Carcassonne.skin = mock(Skin.class);
     }
 
     public void preparePlayers() {
@@ -74,5 +81,125 @@ public class CheatTest {
         assert players.get(0).getNumberOfMeeples() == 0;
         assert players.get(1).getNumberOfMeeples() == 0;
 
+    }
+
+    @Test
+    public void meepleCheatingWithThreePlayers() {
+        preparePlayers();
+        players.add(new Player(3, GameBoard.Color.GREEN, "C"));
+        GameBoard gb = new GameBoard(screenMock, stageMock, stageMock, players, true, players.get(0), null, gameScreen, new GraphicsBackend());
+
+        {
+            gb.cheat(CheatType.MEEPLE, players.get(0), players.get(0));
+
+        }
+        Assert.assertTrue(players.get(0).getNumberOfMeeples() == Player.MEEPLE_COUNT +1);
+        Assert.assertTrue(players.get(1).getNumberOfMeeples() == Player.MEEPLE_COUNT);
+        Assert.assertTrue(players.get(2).getNumberOfMeeples() == Player.MEEPLE_COUNT);
+    }
+
+    @Test
+    public void scoreCheatingWithThreePlayers() {
+        preparePlayers();
+        players.add(new Player(3, GameBoard.Color.GREEN, "C"));
+        GameBoard gb = new GameBoard(screenMock, stageMock, stageMock, players, true, players.get(0), null, gameScreen, new GraphicsBackend());
+
+        {
+            gb.cheat(CheatType.SCORE, players.get(0), players.get(0));
+
+        }
+
+        Assert.assertTrue(players.get(0).getScore() == Player.CHEAT_SCORE);
+        Assert.assertTrue(players.get(1).getScore() == 0);
+        Assert.assertTrue(players.get(2).getScore() == 0);
+    }
+
+    @Test
+    public void detectMeepleCheatingWithThreePlayersRight() {
+        preparePlayers();
+        players.add(new Player(3, GameBoard.Color.GREEN, "C"));
+        GameBoard gb = new GameBoard(screenMock, stageMock, stageMock, players, true, players.get(0), null, gameScreen, new GraphicsBackend());
+
+        {
+            gb.cheat(CheatType.MEEPLE, players.get(0), players.get(0));
+            gb.setCurrentPlayer(players.get(0));
+            gb.cheat(CheatType.MEEPLE, players.get(0), players.get(1)); //detect cheat rightfully
+        }
+
+        Assert.assertTrue(players.get(0).getNumberOfMeeples() == 0);
+        Assert.assertTrue(players.get(1).getNumberOfMeeples() == Player.MEEPLE_COUNT);
+        Assert.assertTrue(players.get(2).getNumberOfMeeples() == Player.MEEPLE_COUNT);
+    }
+
+    @Test
+    public void detectMeepleCheatingWithThreePlayersWrong() {
+        preparePlayers();
+        players.add(new Player(3, GameBoard.Color.GREEN, "C"));
+        GameBoard gb = new GameBoard(screenMock, stageMock, stageMock, players, true, players.get(0), null, gameScreen, new GraphicsBackend());
+
+        {
+            gb.cheat(CheatType.MEEPLE, players.get(0), players.get(0));
+            gb.setCurrentPlayer(players.get(0));
+            gb.cheat(CheatType.MEEPLE, players.get(2), players.get(1)); //detect cheat rightfully
+        }
+
+        Assert.assertTrue(players.get(0).getNumberOfMeeples() == Player.MEEPLE_COUNT +1);
+        Assert.assertTrue(players.get(1).getNumberOfMeeples() == 0);
+        Assert.assertTrue(players.get(2).getNumberOfMeeples() == Player.MEEPLE_COUNT);
+    }
+
+    @Test
+    public void detectScoreCheatingWithThreePlayersRight() {
+        preparePlayers();
+        players.add(new Player(3, GameBoard.Color.GREEN, "C"));
+        GameBoard gb = new GameBoard(screenMock, stageMock, stageMock, players, true, players.get(0), null, gameScreen, new GraphicsBackend());
+
+        {
+            gb.cheat(CheatType.SCORE, players.get(0), players.get(0));
+            gb.setCurrentPlayer(players.get(0));
+            gb.cheat(CheatType.SCORE, players.get(0), players.get(1)); //detect cheat rightfully
+        }
+
+        Assert.assertTrue(players.get(0).getScore() == Player.CHEAT_SCORE * (-2));
+        Assert.assertTrue(players.get(1).getScore() == 0);
+        Assert.assertTrue(players.get(2).getScore() == 0);
+    }
+
+    @Test
+    public void detectScoreCheatingWithThreePlayersWrong() {
+        preparePlayers();
+        players.add(new Player(3, GameBoard.Color.GREEN, "C"));
+        GameBoard gb = new GameBoard(screenMock, stageMock, stageMock, players, true, players.get(0), null, gameScreen, new GraphicsBackend());
+
+        {
+            gb.cheat(CheatType.SCORE, players.get(0), players.get(0));
+            gb.setCurrentPlayer(players.get(0));
+            gb.cheat(CheatType.SCORE, players.get(2), players.get(1)); //detect cheat rightfully
+        }
+
+        Assert.assertTrue(players.get(0).getScore() == Player.CHEAT_SCORE);
+        Assert.assertTrue(players.get(1).getScore() == Player.CHEAT_SCORE *(-3));
+        Assert.assertTrue(players.get(2).getScore() == 0);
+    }
+
+    @Test
+    public void detectCheatingAfterThreeTurn() {
+        preparePlayers();
+        GameBoard gb = new GameBoard(screenMock, stageMock, stageMock, players, true, players.get(0), null, gameScreen, new GraphicsBackendForTests());
+
+        gb.setCurrentPlayer(players.get(0));
+        gb.init();
+
+        TileActor currrent = gb.getCurrentTile();
+        {
+            gb.cheat(CheatType.SCORE, players.get(0), players.get(0));
+            gb.endMyTurn();
+            gb.endMyTurn();
+            gb.endMyTurn();
+            gb.cheat(CheatType.SCORE, players.get(0), players.get(1)); //detect cheat rightfully
+        }
+
+        Assert.assertTrue(players.get(0).getScore() == Player.CHEAT_SCORE);
+        Assert.assertTrue(players.get(1).getScore() == Player.CHEAT_SCORE *(-3));
     }
 }
